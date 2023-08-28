694bb3810d95f14a74cc09b950cc5a45f6f26d2f
Sadik1099 <sadikuzzamanlalin5@gmail.com>
2023-08-28 17:49:35 +0600

    dsadf

---
+++ string.js
@@ -0,0 +1,10 @@
+let text = "qwertyuiopasdfghjklmnbvcxz";
+let length = text.length;
+let part = text.slice(4,14);
+let subss = text.substring(6,17);
+
+
+let replace = " i am a smart boy";
+let newtext = replace.replace("boy", "person");
+
+console.log(newtext);
